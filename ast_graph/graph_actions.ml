5132221e049282e5d1459569c732d701dd07bc46
Izzy <avrisaac555@gmail.com>
2019-06-17 13:46:44 -0700

    Finished the graph_actions implementation

--- ast_graph/graph_actions.ml
+++ ast_graph/graph_actions.ml
@@ -6,7 +6,7 @@ let graph_c (file : Common.filename) =
     if not (file =~ ".*\\c")
     then pr2 "warning: seems not a c file";
 
-    let (xs, stat) = Parse_c.parse_c_and_cpp true false file in
-    xs +> List.iter (fun (ast, (s, toks)) -> 
-
-    )
+    let (tl2, _) = Parse_c.parse_c_and_cpp true false file in
+    let prog = Parse_c.program_of_program2 tl2 in
+    let graph = Dot.graph_from_c_ast prog in
+    pr2 (Dot.string_of_graph graph)
